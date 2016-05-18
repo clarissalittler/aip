d902be34d986a391006048797d3f7b71e1246c11
Clarissa Littler <clarissa.littler@gmail.com>
2016-05-18 11:46:05 -0700

    beep beep

--- gps.lisp
+++ gps.lisp
@@ -1,6 +1,29 @@
 (defvar *state* nil)
 (defvar *ops* nil)
 
+(defparameter *school-ops*
+  (list
+   (make-op :action 'drive-son-to-school
+	    :preconds '(son-at-home car-works)
+	    :add-list '(son-at-school)
+	    :del-list '(son-at-home))
+   (make-op :action 'shop-installs-battery
+	    :preconds '(car-needs-battery shop-knows-problem shop-has-money)
+	    :add-list '(car-works))
+   (make-op :action 'tell-shop-problem
+	    :preconds '(in-communication-with-shop)
+	    :add-list '(shop-knows-problem))
+   (make-op :action 'telephone-shop
+	    :preconds '(know-phone-number)
+	    :add-list '(in-communication-with-shop))
+   (make-op :action 'look-up-number
+	    :preconds '(have-phone-book)
+	    :add-list '(know-phone-number))
+   (make-op :action 'give-shop-number
+	    :preconds '(have-money)
+	    :add-list '(shop-has-money)
+	    :del-list '(have-money))))
+
 (defstruct op
   (action nil) (preconds nil) (add-list nil) (del-list nil))
 
@@ -29,5 +52,3 @@
 
 (defun GPS (*state* goals *ops*)
   (if (every #'achieve goals) 'solved))
-
-
